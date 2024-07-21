8658cf57f888303d84ba3f81eb38a64724c31e28
RamziNaili <naili.ramzi@outlook.fr>
2024-07-21 16:47:55 +0200

    colrised done

--- src/components/Line/Line.jsx
+++ src/components/Line/Line.jsx
@@ -1,67 +1,62 @@
-import clsx from "clsx";
-import styles from "./Line.module.scss";
-import { useState } from "react";
+import clsx from 'clsx';
+import styles from './Line.module.scss';
+import { useEffect, useRef } from 'react';
 
 export const Line = ({ l, guess, word }) => {
+  const ref = useRef(null);
+
   const test = (str) => {
-    console.log(str);
     let i = 0;
     while (i < str.length - 1) {
-      if (!str[i].match(/[a-z]/i)) return true;
+      if (!str[i].match(/[a-z]/i)) return false;
       i++;
     }
-    if (str[i] !== "-") return true;
-    return false;
+    if (str[i] !== '-') return false;
+    return true;
   };
 
-  const lettersColors = (letter, position) => {
-    const t = guess;
+  useEffect(() => {
+    const lettersColors = () => {
+      let tmpWord = word;
 
-    if (!letter) return;
-    // if (
-    //   guess.length < 6 ||
-    //   !guess[0 + l * 6] ||
-    //   !guess[0 + l * 6].match(/[a-z]/i) ||
-    //   !guess[1 + l * 6] ||
-    //   !guess[1 + l * 6].match(/[a-z]/i) ||
-    //   !guess[2 + l * 6] ||
-    //   !guess[2 + l * 6].match(/[a-z]/i) ||
-    //   !guess[3 + l * 6] ||
-    //   !guess[3 + l * 6].match(/[a-z]/i) ||
-    //   !guess[4 + l * 6] ||
-    //   !guess[4 + l * 6].match(/[a-z]/i) ||
-    //   guess[5 + l * 6] ||
-    //   guess[5 + l * 6] !== ""
-    // )
-    //   return;
-    console.log();
-    if (test(guess.join("").substring(0 + l * 6, 5 + l * 6))) return;
+      let i = 0;
+      while (i < ref.current.children.length) {
+        const letter = ref.current.children[i].textContent;
+        if (tmpWord.includes(letter) && tmpWord[i] === letter) {
+          ref.current.children[i].classList.add(styles.green);
+          tmpWord = tmpWord.replace(letter, '-');
+        }
+        i++;
+      }
+      i = 0;
+      while (i < ref.current.children.length) {
+        const letter = ref.current.children[i].textContent;
+        if (tmpWord.includes(letter)) {
+          ref.current.children[i].classList.add(styles.orange);
+          tmpWord = tmpWord.replace(letter, '-');
+        } else ref.current.children[i].classList.add(styles.grey);
+        i++;
+      }
+    };
 
-    if (word.includes(letter)) {
-      console.log(t.join("").slice(-5), letter, position);
-      if (word[position] === letter) return styles.green;
-      return styles.orange;
-    }
-    return styles.grey;
-  };
+    if (test(guess.join('').substring(0 + l * 6, 6 + l * 6))) lettersColors();
+  }, [guess, l, word]);
 
   return (
-    <div className={styles.lines}>
-      <div className={clsx(styles.box, lettersColors(guess[0 + l * 6], 0))}>
-        {guess[0 + l * 6]}
-      </div>
-      <div className={clsx(styles.box, lettersColors(guess[1 + l * 6], 1))}>
-        {guess[1 + l * 6]}
-      </div>
-      <div className={clsx(styles.box, lettersColors(guess[2 + l * 6], 2))}>
-        {guess[2 + l * 6]}
-      </div>
-      <div className={clsx(styles.box, lettersColors(guess[3 + l * 6], 3))}>
-        {guess[3 + l * 6]}
-      </div>
-      <div className={clsx(styles.box, lettersColors(guess[4 + l * 6], 4))}>
-        {guess[4 + l * 6]}
-      </div>
+    <div className={styles.lines} ref={ref}>
+      <div className={clsx(styles.box)}>{guess[0 + l * 6]}</div>
+      <div className={clsx(styles.box)}>{guess[1 + l * 6]}</div>
+      <div className={clsx(styles.box)}>{guess[2 + l * 6]}</div>
+      <div className={clsx(styles.box)}>{guess[3 + l * 6]}</div>
+      <div className={clsx(styles.box)}>{guess[4 + l * 6]}</div>
     </div>
   );
 };
+
+// if (!test(guess.join('').substring(0 + l * 6, 6 + l * 6))) return;
+
+// if (word.includes(letter)) {
+//   if (word[position] === letter) return styles.green;
+//   return styles.orange;
+// }
+// return styles.grey;
